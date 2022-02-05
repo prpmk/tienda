5888c11a564ec6bc3f330a0cf0b4120b5669c943
erik-quintero <l171080151@iztapalapa.tecnm.mx>
2022-02-04 23:25:11 -0600

    linea master

---
+++ database/migrations/2022_02_04_164743_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',255);
+            $table->string('slug');
+            $table->text('description');
+            $table->string('extract');
+            $table->decimal('price', 5, 2);
+            $table->string('image',300);
+            $table->boolean('visible');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('cascade');
+            //$table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
